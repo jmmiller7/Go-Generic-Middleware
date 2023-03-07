270249b58d500de5c26cee8f129ad9aceb84abc1
Jacob Miller <jake.miller@tackle.io>
2023-03-07 09:19:19 -0600

    add a bunrouter middleware wrapper

--- bunrouter/bunrouter.go
+++ bunrouter/bunrouter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"generic-middleware/middleware"
 	"log"
 	"net/http"
@@ -21,34 +22,58 @@ func realMain() error {
 
 	logger.Info("starting api...")
 
-	router := bunrouter.New(bunrouter.Use(NewLoggerMiddleware(logger)))
-	router.GET("/", Get)
+	router := bunrouter.New(
+		bunrouter.Use(NewErrorLoggingMiddleware(logger)),
+		bunrouter.Use(WrapMiddleware(middleware.NewRequestLoggingMiddleware(logger))),
+	)
+	router.GET("/", get)
+	router.GET("/error", getWithError)
 
 	host := "localhost:5050"
 	logger.Sugar().Infof("api running on %s\n", host)
 	return http.ListenAndServe(host, router)
 }
 
-func NewLoggerMiddleware(logger *zap.Logger) bunrouter.MiddlewareFunc {
+func NewErrorLoggingMiddleware(logger *zap.Logger) func(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
 	return func(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
-		// first, we need to convert "next" to a http.HandlerFunc to plug it into the middleware
-		f := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// RISK: we will no longer see a returned error here
-			next.ServeHTTP(w, r)
-		})
+		return func(w http.ResponseWriter, r bunrouter.Request) error {
+			/*
+				some panic handling here...
+			*/
 
-		// then, we can init the new middleware piece
-		h := middleware.NewRequestLoggingMiddleware(logger)
-
-		// finally we can plug the middleware into bunrouter
-		return bunrouter.HTTPHandler(h(f))
+			err := next(w, r)
+			if err != nil {
+				logger.Error("an error occurred!", zap.Error(err))
+				resp := http.Response{
+					StatusCode: http.StatusInternalServerError,
+				}
+				resp.Write(w)
+			}
+			return nil
+		}
 	}
 }
 
-func Get(w http.ResponseWriter, req bunrouter.Request) error {
+func WrapMiddleware(m func(http.Handler) http.Handler) bunrouter.MiddlewareFunc {
+	return func(next bunrouter.HandlerFunc) bunrouter.HandlerFunc {
+		return func(w http.ResponseWriter, r bunrouter.Request) (err error) {
+			m(
+				http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+					err = next(w, r)
+				})).ServeHTTP(w, r.Request)
+			return
+		}
+	}
+}
+
+func get(w http.ResponseWriter, req bunrouter.Request) error {
 	r := new(http.Response)
 	r.StatusCode = http.StatusOK
 
 	_, err := w.Write([]byte("everything is awesome on bunrouter!"))
 	return err
+}
+
+func getWithError(w http.ResponseWriter, req bunrouter.Request) error {
+	return errors.New("an error occurred")
 }
